1f2da80a871279658ba92231f6bd06372cd66476
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-01-29 22:38:53 +0000

    add overlay

--- godot/Game.gd
+++ godot/Game.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 onready var FirePlayer = $WorldObjects/Fire
 onready var FrostPlayer = $WorldObjects/Frost
+onready var overlay = $UI/Overlay
 
 func _ready():
 	FirePlayer.connect("character_dead",self, "_playeronedeath")
 	FrostPlayer.connect("character_dead",self, "_playertwodeath")
+	overlay.fade_in()
 
 func _playeronedeath():
 	print("Player one died")
