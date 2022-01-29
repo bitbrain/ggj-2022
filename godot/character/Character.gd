b3b76d728ae7667346576d49ef3464d8dcb254e2
Isaac Furieri <62474855+isaacfurieri@users.noreply.github.com>
2022-01-29 17:27:37 +0000

    implemented charging

--- godot/character/Character.gd
+++ godot/character/Character.gd
@@ -5,22 +5,26 @@ enum PlayerType {
 	FIRE
 	FROST
 }
-
 export(float) var ACCELERATION = 340
+export(float) var CHARGING_ACCELERATION = ACCELERATION * 3
 export(float) var FRICTION = 670
 export(float) var MAX_SPEED = 175
+export(float) var CHARGING_SPEED = MAX_SPEED * 3
 export(PlayerType) var PLAYER_TYPE
 
 export(String) var east_action
 export(String) var west_action
 export(String) var south_action
 export(String) var north_action
+export(String) var charge_action
 
 onready var FireSprite = $FireSprite
 onready var FrostSprite = $FrostSprite
+onready var ChargerTimer = $Timer
 
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
+var charging = false
 
 func _ready():
 	if PLAYER_TYPE == PlayerType.FIRE:
@@ -31,8 +35,19 @@ func _ready():
 		FrostSprite.visible = true
 
 func _physics_process(delta):
-	input_vector.x = Input.get_action_strength(east_action) - Input.get_action_strength(west_action)
-	input_vector.y = Input.get_action_strength(south_action) - Input.get_action_strength(north_action)
-	input_vector = input_vector.normalized()
-	velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	if Input.is_action_just_pressed(charge_action) and not charging:
+		charging = true
+		ChargerTimer.start()
+		
+	if not charging:
+		input_vector.x = Input.get_action_strength(east_action) - Input.get_action_strength(west_action)
+		input_vector.y = Input.get_action_strength(south_action) - Input.get_action_strength(north_action)
+		input_vector = input_vector.normalized()
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(input_vector * CHARGING_SPEED, CHARGING_ACCELERATION * delta)
 	velocity = move_and_slide(velocity)
+
+
+func _on_Timer_timeout():
+	charging = false
--- godot/tree/Tree.gd
+++ godot/tree/Tree.gd
@@ -5,9 +5,9 @@ onready var treeSprite = $Sprite
 
 func _on_Area2D_body_entered(body):
 	var character = body as Character
-	if character.PLAYER_TYPE == Character.PlayerType.FIRE:
+	if character.PLAYER_TYPE == Character.PlayerType.FIRE and character.charging:
 		treeSprite.frame = 1
-	if character.PLAYER_TYPE == Character.PlayerType.FROST:
+	if character.PLAYER_TYPE == Character.PlayerType.FROST and character.charging:
 		treeSprite.frame = 2	
 	#print ("Collision with tree detected")
 	
