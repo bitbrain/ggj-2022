3cfe79669778423bdab44322124843debe714a9c
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-01-30 13:19:40 +0000

    add health bar

--- godot/ui/CharacterUI.gd
+++ godot/ui/CharacterUI.gd
@@ -6,6 +6,10 @@ onready var character = get_node(character_path) as Character
 onready var energySprite = $Energy
 onready var health = $Health
 onready var animation_player = $AnimationPlayer
+onready var health_overlay = $HealthOverlay
+onready var tween = $Tween
+
+var current_scale = 0
 
 func _ready():
 	character.connect("health_changed", self, "_on_health_changed")
@@ -15,9 +19,20 @@ func _ready():
 		animation_player.play("health-fire")
 	if character.PLAYER_TYPE == Character.PlayerType.FROST:
 		animation_player.play("health-ice")
+		
+func _process(delta):
+	health_overlay.scale.x = current_scale
 
 func _on_health_changed(health):
-	pass
+	var inverseHealth = 100 - health
+	var target_scale = 1
+	if inverseHealth != 0:
+		target_scale = inverseHealth / 100
+	else:
+		target_scale = 0
+	tween.stop_all()
+	tween.interpolate_property(self, "current_scale", current_scale, target_scale, 0.3, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	tween.start()
 
 func _on_energy_changed(energy):
 	energySprite.frame = energy
