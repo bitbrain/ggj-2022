214761c90c98ae46a2c3541ae371d46f20906cec
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-01-30 14:35:05 +0000

    add UI sounds

--- godot/ui/GameButton.gd
+++ godot/ui/GameButton.gd
@@ -3,6 +3,8 @@ extends TextureButton
 export(String) var text
 
 onready var label : Label = $Label
+onready var hover_sound = $HoverSound
+onready var click_sound = $ClickSound
 
 func _ready():
 	label.modulate = Color("a6859f")
@@ -16,8 +18,12 @@ func _process(delta):
 
 
 func _on_GameButton_mouse_entered():
+	hover_sound.play()
 	label.modulate = Color("d9bdc8")
 
 
 func _on_GameButton_mouse_exited():
 	label.modulate = Color("a6859f")
+
+func _on_GameButton_pressed():
+	click_sound.play()
